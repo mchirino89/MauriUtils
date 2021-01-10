79bf34b94d14538fdfb8d707a85441b1771bd2dd
Mauricio Chirino <1657723+mchirino89@users.noreply.github.com>
2021-01-09 22:19:48 -0300

    Include syntactic sugar for UI thread handling (#5)
    * Include syntactic sugar for UI thread handling
    
    * Remove redundat boilerplate
    
    * Remove UIKit code

---
+++ Sources/MauriUtils/Helpers/SyntacticSugar.swift
@@ -0,0 +1,19 @@
+//
+//  SyntacticSugar.swift
+//
+//
+//  Created by Mauricio Chirino on 14/12/20.
+//
+
+import Foundation
+
+/// Performs an update on main thread safely
+/// - Parameter closure: closure with update execution
+func executeMainThreadUpdate(using closure: @escaping () -> Void) {
+    // If we're already on the main thread, execute it directly
+    if Thread.isMainThread {
+        closure()
+    } else {
+        DispatchQueue.main.async(execute: closure)
+    }
+}
---
+++ Tests/MauriUtilsTests/Helpers/SyntacticSugarTestCases.swift
@@ -0,0 +1,62 @@
+//
+//  SyntacticSugarTestCases.swift
+//  
+//
+//  Created by Mauricio Chirino on 04/01/21.
+//
+
+import XCTest
+@testable import MauriUtils
+
+final class SyntacticSugarTestCases: XCTestCase {
+    var expectation: XCTestExpectation!
+    var dummyArray: [Int]!
+
+    override func setUp() {
+        super.setUp()
+        expectation = expectation(description: "Update on main thread safely")
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        expectation = nil
+        dummyArray.removeAll()
+    }
+
+    func testSafelyBackgroundThreadUpdate() {
+        givenInitialSetup()
+        whenUpdateIsExecutedInBackgroundThread()
+        thenVerifyProperUpdate()
+    }
+
+    func testDirectMainThreadRun() {
+        givenInitialSetup()
+        whenOperationIsExecutedInTheMainThread()
+        thenVerifyProperUpdate()
+    }
+}
+
+private extension SyntacticSugarTestCases {
+    func givenInitialSetup() {
+        dummyArray = []
+    }
+
+    func whenUpdateIsExecutedInBackgroundThread() {
+        DispatchQueue.global(qos: .background).async { [unowned self] in
+            whenOperationIsExecutedInTheMainThread()
+        }
+    }
+
+    func whenOperationIsExecutedInTheMainThread() {
+        executeMainThreadUpdate { [unowned self] in
+            dummyArray.append(contentsOf: [1, 2, 3])
+            expectation.fulfill()
+        }
+    }
+
+    func thenVerifyProperUpdate() {
+        waitForExpectations(timeout: 0.01) { [unowned self] _ in
+            XCTAssertEqual(dummyArray.count, 3)
+        }
+    }
+}
